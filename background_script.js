2adccf1dcc9dfa05cb4972f6acb2992130461413
dillingspam <99294553+dillingspam@users.noreply.github.com>
2022-02-14 21:25:45 -0500

    Updated to Manifest V3

--- background_script.js
+++ background_script.js
@@ -1,55 +1,87 @@
-var signIn = false; // initialize token info
+let ruleList = false;
 
 // set signIn if cookie found
 function cookieStatus(cookie) {
     if (cookie) {
+        console.log("found");
         if (cookie.domain === ".amazon.com") {
-            signIn = true;
+            if (ruleList === false) {
+                chrome.declarativeNetRequest.updateSessionRules({
+                    addRules: [
+                        {
+                            "id": 1,
+                            "priority": 1,
+                            "action": {
+                                "type": "redirect",
+                                "redirect": {
+                                    "transform": { "scheme": "https", "host": "smile.amazon.com" }
+                                }
+                            },
+                            "condition": { "urlFilter": "*www.amazon.com*", "resourceTypes": ["main_frame"] }
+                        }
+                    ]
+                })
+                for (x = 2; x > 0; x--) {
+                    chrome.tabs.query({ active: true, currentWindow: true }).then((tab) => chrome.tabs.reload(tab[0].id));
+                    console.log("reloading");
+                }
+
+            }
         }
     }
+
     else {
-        signIn = false;
+        if (ruleList === true) {
+            chrome.declarativeNetRequest.updateSessionRules({ removeRuleIds: [1] });
+            for (x = 2; x > 0; x--) {
+                chrome.tabs.query({ active: true, currentWindow: true }).then((tab) => chrome.tabs.reload(tab[0].id));
+                console.log("reloading");
+            }
+        }
+        else {
+            console.log("not found");
+            chrome.declarativeNetRequest.updateSessionRules({ removeRuleIds: [1] });
+        }
+
     }
 }
 
 // check for cookie based on window type then send promise getting to cookieStatus
 function getCookie(window) {
     if (window.incognito === true) {
-        chrome.cookies.get({ name: "sess-at-main", url: "https://www.amazon.com", storeId: "1" }, cookieStatus);
+        getting = chrome.cookies.get({ name: "sess-at-main", url: "https://www.amazon.com", storeId: "1" });
     }
     else {
-        chrome.cookies.get({ name: "sess-at-main", url: "https://www.amazon.com" }, cookieStatus);
+        getting = chrome.cookies.get({ name: "sess-at-main", url: "https://www.amazon.com" });
+    }
+    getting.then(cookieStatus);
+}
+
+function ruleCheck(Rule) {
+    if (Rule.length !== 0) {
+        if (Rule[0].id === 1) {
+            ruleList = true;
+        }
+    }
+    else {
+        ruleList = false
     }
 }
 
 // get current window to determine if private browsing
-function getActiveWindow(details) {
-    chrome.windows.getCurrent(getCookie);
-    void (details);
+function getActiveWindow() {
+    ruleList = false;
+    chrome.declarativeNetRequest.getSessionRules().then(ruleCheck);
+    gettingWindow = chrome.windows.getCurrent();
+    gettingWindow.then(getCookie);
 }
 
-// redirect amazon when token is true
-function redirect(requestDetails) {
-    console.log(signIn);
-    if (signIn === true) {
-        signIn = false;
-        console.log("Redirecting: " + requestDetails.url);
-        return { redirectUrl: requestDetails.url.replace('https://www.amazon.com', 'https://smile.amazon.com') };
-    }
-}
-
-chrome.webRequest.onBeforeRedirect.addListener(
+chrome.webNavigation.onBeforeNavigate.addListener(
     getActiveWindow,
-    { urls: ["https://www.amazon.com/*"], types: ["main_frame"] }
-);
+    { url: [{ hostEquals: "www.amazon.com" }] }
+)
 
-chrome.webRequest.onHeadersReceived.addListener(
-    redirect,
-    { urls: ["https://www.amazon.com/*"] },
-    ["blocking"]
-);
-
-chrome.webRequest.onBeforeRequest.addListener(
+chrome.webNavigation.onCommitted.addListener(
     getActiveWindow,
-    { urls: ["https://www.amazon.com/*"], types: ["main_frame"] },
-);
+    { url: [{ hostEquals: "www.amazon.com" }] }
+)
